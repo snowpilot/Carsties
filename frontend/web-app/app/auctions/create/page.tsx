039f931d5a5fc5de23c511954be37d21e97df6e6
Wolfgang Fellenz <wolfgang@fellenz.de>
2024-03-18 16:10:38 +0100

    End of section 14

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -6,7 +6,24 @@ export default function Create() {
   return (
     <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
       <Heading title='Sell your car!' subtitle='Please enter the details of your car' />
-      <AuctionForm />
+      <AuctionForm auction={{
+        reservePrice: 0,
+        seller: '',
+        winner: undefined,
+        soldAmount: 0,
+        currentHighBid: 0,
+        createdAt: '',
+        updatedAt: '',
+        auctionEnd: '',
+        status: '',
+        make: '',
+        model: '',
+        year: 0,
+        color: '',
+        mileage: 0,
+        imageUrl: '',
+        id: ''
+      }} />
     </div>
   )
 }
--- frontend/web-app/app/lib/fetchWrapper.ts
+++ frontend/web-app/app/lib/fetchWrapper.ts
@@ -1,5 +1,5 @@
 import { getTokenWorkaround } from '@/app/actions/authActions';
-const baseUrl = 'http://localhost:6001/'
+const baseUrl = process.env.API_URL;
 
 async function get(url: string) {
     const requestOptions = {
